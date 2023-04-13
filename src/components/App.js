085e9571f29ac83b6bc9a93b4296b40547bada79
Samuel Lakra <samuellakra1290@gmail.com>
2023-04-13 17:46:19 +0530

    1st commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,41 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "../styles/App.css";
 import User from "../models/user";
 
 const App = () => {
+//for signup
+const nameInput = useRef();
+const emailInput = useRef();
+const passwordInput= useRef();
+const [nameData ,setNameData] = useState('');
+const [emailData ,setEmailData] = useState('');
+const [passwordData ,setPasswordData] = useState('');
+
+//for login
+const loginEmail = useRef('');
+const loginPassword = useRef('');
+const [name ,setName] = useState('');
+const [email ,setEmail] = useState('');
+const [password ,setPassword] = useState('');
+
+const signUpForm = document.querySelector(".beforeLogin");
+
+  function handleSignup(){
+    setNameData(nameInput.current.value);
+    setEmailData(emailInput.current.value);
+    setPasswordData(passwordInput.current.value);
+  }
+
+
+  function handleLogin(){
+    // console.log(loginEmail.current.value)
+    // console.log(emailData)
+    if(loginEmail.current.value == emailData && loginPassword.current.value == passwordData){
+      setName(nameInput.current.value);
+      setEmail(loginEmail.current.value);
+      setPassword(loginPassword.current.value);
+    }
+  }
   return (
     <div id="main">
       <table id="all-users">
@@ -13,43 +46,40 @@ const App = () => {
             <th>Password</th>
           </tr>
           <tr>
-            <td>{/** user's name */}</td>
-            <td>{/** user's email */}</td>
-            <td>{/** user's password */}</td>
+            <td>{nameData}</td>
+            <td>{emailData}</td>
+            <td>{passwordData}</td>
           </tr>
         </tbody>
       </table>
 
-      <div>
+      <div className="beforeLogin">
         <form className="signup-form">
           <label htmlFor="name">Name</label>
-          <input type="text" name="signupName" id="signupName" />
+          <input type="text" name="signupName" id="signupName" ref={nameInput}/>
           <label htmlFor="email">Email</label>
-          <input type="email" name="signupEmail" id="signupEmail" />
+          <input type="email" name="signupEmail" id="signupEmail" ref={emailInput}/>
           <label htmlFor="password">Password</label>
-          <input type="password" name="signupPassword" id="signupPassword" />
+          <input type="password" name="signupPassword" id="signupPassword" ref={passwordInput}/>
           <label htmlFor="confirmPassword">Confirm Password</label>
-          <input
-            type="password"
-            name="signupConfirmPassword"
-            id="signupConfirmPassword"
-          />
+          <input type="password" name="signupConfirmPassword" id="signupConfirmPassword"/>
         </form>
-        <button id="signup-button">Signup</button>
+        <button id="signup-button" onClick={handleSignup}>Signup</button>
         <form className="login-styles">
           <label htmlFor="loginEmail">Email</label>
-          <input id="loginEmail" name="loginEmail" type="email" />
+          <input id="loginEmail" name="loginEmail" type="email" ref={loginEmail}/>
           <label htmlFor="loginPassword">Password</label>
-          <input id="loginPassword" name="loginPassword" type="password" />
+          <input id="loginPassword" name="loginPassword" type="password" ref={loginPassword}/>
         </form>
-        <button id="login-button">Login</button>
+        <button id="login-button" onClick={handleLogin}>Login</button>
       </div>
 
-      <div>
-        <h3 id="username">{/** Logged in user's name */}</h3>
-        <h3 id="email">{/** Logged in user's email */}</h3>
+      <div className="afterLogin">
+        <h3 id="username">{name}</h3>
+        <h3 id="email">{email}</h3>
         <button id="logout-button">Logout</button>
       </div>
+      
     </div>
   );
 };
